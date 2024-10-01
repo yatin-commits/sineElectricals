8d32a2bce2995379544fb7882c2b2d9260e38ef4
YATIN SHARMA <sharmayatin0882@gmail.com>
2024-10-01 11:16:50 +0530

    s

--- sin/vite.config.js
+++ sin/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-  alias: {
-    "@": path.resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    }
   },
-})
+  server: {
+    historyApiFallback: true, // This helps with client-side routing
+  }
+});
