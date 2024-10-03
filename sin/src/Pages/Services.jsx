c770055012359a2e3555f43bde9617a9354edf05
YATIN SHARMA <sharmayatin0882@gmail.com>
2024-10-03 22:56:42 +0530

    added ind we serve

--- sin/src/Pages/Services.jsx
+++ sin/src/Pages/Services.jsx
@@ -2,8 +2,10 @@
 // import productsData from '../Ser'; // Adjust the path as needed
 import { ServiceData } from '../../ServicesData';
 
+// import './services.css'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import IndustriedWeServe from '../components/IndustriedWeServe';
 
 const Services = () => {
   return (
@@ -15,7 +17,10 @@ const Services = () => {
         {/* Products List */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center text-center m-auto">
           {ServiceData.map((product) => (
-            <div key={product.id} className="border p-4 rounded-lg  flex flex-col justify-center items-center">
+            <div
+              key={product.id}
+              className="border p-4 rounded-lg flex flex-col justify-center items-center shadow-md hover:shadow-lg transform hover:-translate-y-1 transition duration-300"
+            >
               <img
                 src={product.image}
                 alt={product.name}
@@ -27,6 +32,47 @@ const Services = () => {
           ))}
         </div>
       </div>
+
+      {/* Industries We Serve */}
+      <h1 className="text-4xl font-semibold m-4 items-center text-center">Industries We Serve</h1>
+      <div className='flex justify-evenly flex-wrap flex-row w-full '>
+            <IndustriedWeServe title="AeroSpace"/>
+            <IndustriedWeServe title="AirPorts" />
+            {/* <IndustriedWeServe title="Auto. Manufacturing"/> */}
+            <IndustriedWeServe title="Battery "/>
+            <IndustriedWeServe title="Chemical Inductry"/>
+            <IndustriedWeServe title="Cement"/>
+            <IndustriedWeServe title="Cranes"/>
+            <IndustriedWeServe title="Water Industry"/>
+            <IndustriedWeServe title="Wind"/>
+            <IndustriedWeServe title="Higher Education"/>
+            <IndustriedWeServe title="Semi  conductors"/>
+            {/* <IndustriedWeServe title="Smart Urban Communities"/> */}
+            <IndustriedWeServe title="Tire Industry"/>
+            <IndustriedWeServe title="Transport & Logistics"/>
+            <IndustriedWeServe title="Data Centers"/>
+            <IndustriedWeServe title="Pulp & Paper"/>
+            <IndustriedWeServe title="Power Utilities "/>
+            <IndustriedWeServe title="Cranes"/>
+            <IndustriedWeServe title="Distributors"/>
+            <IndustriedWeServe title="Electronic Industry"/>
+            <IndustriedWeServe title="Food & Beverages Industry"/>
+            <IndustriedWeServe title="Glass & Solar"/>
+            {/* <IndustriedWeServe title="Healthcare"/> */}
+            <IndustriedWeServe title="Heavy Equipments"/>
+            <IndustriedWeServe title="Healthcare Industry"/>
+            <IndustriedWeServe title="Power Utilities"/>
+            {/* <IndustriedWeServe title="Health"/> */}
+            <IndustriedWeServe title="Intra logistics"/>
+            <IndustriedWeServe title="Mining Industry"/>
+            <IndustriedWeServe title="Oil & Gas Industry"/>
+            
+            
+
+        
+        
+      </div>
+
       <Footer />
     </>
   );
---
+++ sin/src/components/IndustriedWeServe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IndustriedWeServe = (props) => {
+  return (
+    <>
+    <div className='shadow-md overflow-clip items-center  justify-center  font-semibold hover:shadow-xl border-1  border-gray-300 w-[25%] text-center m-2 text-lg transform hover:-translate-y-1 transition  duration-300 px-2 py-2 rounded-lg'>
+        {props.title}
+    </div>
+    </>
+  )
+}
+
+export default IndustriedWeServe
